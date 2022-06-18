b888a11f920bc4dc5716a777c43b0a531bc8de71
Bassel Hakim <bassel.safwat92@gmail.com>
2022-06-18 16:18:39 -0400

    level 1 - day 7

---
+++ leetcode/binary_search/704. Binary Search.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+
+            if target == nums[middle]:
+                return middle
+
+            if target > nums[middle]:
+                left = middle + 1
+            else:
+                right = middle - 1
+
+        return -1
